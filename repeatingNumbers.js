fe8ef26abc6b2f50eeaa332f0ef422aa982ec3c5
AnthonyA <alicea.9a@gmail.com>
2024-11-03 21:23:20 -0700

    Added function that takes nested arrays and returns a str of repeating numbers based on the arrays elements

---
+++ repeatingNumbers.js
@@ -0,0 +1,26 @@
+const repeatNumbers = function (data) {
+  let str = [];
+    for (const array of data){
+     for (let i = 0; i < array[1]; i++) {
+      str.push(array[0]);
+     }
+     str.push(',')
+    }
+  str.pop()
+  return str.join('');  
+};
+
+console.log(repeatNumbers([[1, 10]]));
+console.log(
+  repeatNumbers([
+    [1, 2],
+    [2, 3],
+  ])
+);
+console.log(
+  repeatNumbers([
+    [10, 4],
+    [34, 6],
+    [92, 2],
+  ])
+);
